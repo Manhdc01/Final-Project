1337f9639ca19d591446349eb80a5a546c1aff3a
Manhdc01 <manhnhgch190771@fpt.edu.vn>
2024-04-15 19:24:30 +0700

    update session

--- src/app.js
+++ src/app.js
@@ -25,7 +25,12 @@ app.use(session({
     secret: process.env.SECRET_SESSION,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Nếu bạn không sử dụng HTTPS, đặt giá trị này thành false
+    cookie: {
+        secure: true, // Đảm bảo sử dụng HTTPS
+        httpOnly: true, // Ngăn chặn truy cập từ mã JavaScript
+        maxAge: 1000 * 60 * 60 * 24, // Thời gian sống của cookie (ở đây là 1 ngày)
+        sameSite: 'strict' // Ngăn chặn các yêu cầu giữa các trang từ trang web khác
+    } // Nếu bạn không sử dụng HTTPS, đặt giá trị này thành false
 }));
 //config req.body
 app.use(express.json())
@@ -47,11 +52,8 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-// set route for static file
-// app.use('/public/images/upload', express.static(path.join(__dirname, '/public/images/upload')))
 
 app.use('/', webRoutes)
-
     ; (async () => {
         //test connection
         try {
